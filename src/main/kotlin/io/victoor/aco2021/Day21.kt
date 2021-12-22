9b02e2b8d4858a3f253348e1699628aa2fda7a0d
Jelle Victoor <jelle.victoor@conundra.eu>
2021-12-22 21:01:25 +0100

    day 21 ️⭐️⭐️ Investigated dynamic programming and looked at examples on how this could work and the reimplemented this myself

--- src/main/kotlin/io/victoor/aco2021/Day21.kt
+++ src/main/kotlin/io/victoor/aco2021/Day21.kt
@@ -1,7 +1,14 @@
 package io.victoor.aco2021
 
-import io.victoor.aco2021.model.game.Game
-import io.victoor.aco2021.model.game.Player
+
+data class Player(val name: String, val score: Int, val position: Int) {
+    fun roll(dieRollValue: Int): Player {
+        // position is 0 based, score is 1 based
+        val newPosition = (position - 1 + dieRollValue) % 10 + 1
+        val newScore = score + newPosition
+        return Player(name, newScore, newPosition)
+    }
+}
 
 class Day21 : SolutionExecutor {
     override fun process(input: List<String>): Number {
@@ -14,9 +21,6 @@ class Day21 : SolutionExecutor {
     fun playGame(
         players: List<Player>
     ): Number {
-        val game = Game(
-            10, players
-        )
         val universes = playTurn(players[0], players[1], mutableMapOf())
         return universes.toList().maxOf { it }
     }
--- src/main/kotlin/io/victoor/aco2021/model/game/GamePosition.kt
+++
@@ -1,3 +0,0 @@
-package io.victoor.aco2021.model.game
-
-data class GamePosition(var position:Int)
